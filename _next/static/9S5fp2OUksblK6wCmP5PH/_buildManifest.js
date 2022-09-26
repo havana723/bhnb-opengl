2f9bf589e193bea29250ba5b7eb1291ed5777a15
havana723 <havana723@users.noreply.github.com>
2022-09-26 13:10:17 +0000

    deploy: 3b223336dbbb88ba7816c9516c3fbb42f433b507

---
+++ _next/static/9S5fp2OUksblK6wCmP5PH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-b941061d70e99b43.js","static/chunks/810-7d98e416d08ec9ab.js","static/chunks/pages/index-616f7a2776bda0cf.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
