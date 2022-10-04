b2fa05a35e540df642ec2fc4ea39ce2431002fef
havana723 <havana723@users.noreply.github.com>
2022-10-04 11:17:24 +0000

    deploy: 78e8c7098da45a5c22eb22e0d39a8805f934466c

---
+++ _next/static/GEXP4s9mPPfUOUir3PjeJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-b941061d70e99b43.js","static/chunks/484-15b74a40d9ed1348.js","static/chunks/pages/index-989a169547795041.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
